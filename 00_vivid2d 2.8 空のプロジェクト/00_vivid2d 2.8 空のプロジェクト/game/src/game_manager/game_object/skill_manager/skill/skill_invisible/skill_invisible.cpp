930a9aa0fa4b52ffaa98219c405727be0bd061e6
1zxcvbnm8 <130630783+1zxcvbnm8@users.noreply.github.com>
2024-12-06 14:08:39 +0900

    透明スキル進行中

--- 00_vivid2d 2.8 空のプロジェクト/00_vivid2d 2.8 空のプロジェクト/game/src/game_manager/game_object/skill_manager/skill/skill_invisible/skill_invisible.cpp
+++ 00_vivid2d 2.8 空のプロジェクト/00_vivid2d 2.8 空のプロジェクト/game/src/game_manager/game_object/skill_manager/skill/skill_invisible/skill_invisible.cpp
@@ -1,5 +1,8 @@
 #include "skill_invisible.h"
 
+const float CSkillInvisible::m_cool_time = 5.0f;
+const float CSkillInvisible::m_invisible_time = 5.0f;
+
 CSkillInvisible::CSkillInvisible(void)
 	:CSkill()
 	,m_State(STATE::WAIT)
@@ -15,23 +18,31 @@ void CSkillInvisible::Initialize(CPlayer* player)
 {
 	CSkill::Initialize(player);
 	m_State = STATE::WAIT;
-	m_Timer.SetUp(m_invisibletime);
+	m_Timer.SetUp(m_invisible_time);
 }
 
 void CSkillInvisible::Update(void)
 {
 	CSkill::Update();
+	m_Timer.Update();
 
 	switch (m_State)
 	{
 	case CSkillInvisible::STATE::WAIT:
 		break;
 	case CSkillInvisible::STATE::INVISIBLE:
-		m_Timer.Update();
 
+		if (m_Timer.Finished())
+		{
+			m_State = CSkillInvisible::STATE::IS_COOL_TIME;
+			m_Timer.Reset();
+		}
 
 		break;
 	case CSkillInvisible::STATE::IS_COOL_TIME:
+		
+		
+
 		break;
 	default:
 		break;
@@ -46,4 +57,10 @@ void CSkillInvisible::Draw(void)
 void CSkillInvisible::Finalize(void)
 {
 	CSkill::Finalize();
+}
+
+void CSkillInvisible::Action()
+{
+	m_State = CSkillInvisible::STATE::INVISIBLE;
+	m_Timer.SetUp(m_invisible_time);
 }
